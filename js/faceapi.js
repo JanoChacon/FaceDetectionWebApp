fc47ec1d4fb3ab995371b2a491f02103bd830a1e
Alejandro <a.chacon01@ufromail.cl>
2018-10-16 23:51:33 -0300

    acomodo de archivos

--- js/faceapi.js
+++ js/faceapi.js
@@ -3,7 +3,6 @@ var API_SECRET = "g8d6IiLW7GfQyfWKh5oTOSjsMvL5SjZr";
 var API_URL = "https://api-us.faceplusplus.com/facepp/v3";
 
 function detectarRostro(source, sourceType) {
-
     var ATRIBUTOS = "facequality,gender";
     var xhr = new XMLHttpRequest();
     xhr.timeout = 10 * 1000;
@@ -33,24 +32,38 @@ function detectarRostro(source, sourceType) {
     fd.append('' + sourceType, source);
     xhr.send(fd);
 
-    /* 
-    $.ajax({
-             url: API_URL + '/detect',
-             type: "POST",
-             crossDomain: true,
-             data: {
-                 api_key: API_KEY,
-                 api_secret: API_SECRET,
-                 url: sourceImageUrl
-             },
-             xhrFields: {
-                 withCredentials: true
-             },
-             contentType: 'application/x-www-form-urlencoded',
-             success: function (response) {
-             $("#responseTextArea").val(JSON.stringify(data, null, 2));
-             },
-             timeout: 10 * 1000
-         });
-     */
-}; 
+};
+
+function compararRostros(source1, source1type, source2, source2type) {
+    var ATRIBUTOS = "";
+    var xhr = new XMLHttpRequest();
+    xhr.timeout = 10 * 1000;
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var json = JSON.stringify(JSON.parse(xhr.responseText), null, '  ');
+                try {
+                    $("#responseTextArea").html(html);
+                    //return json;
+                } catch (err) {
+                    $("#responseTextArea").text(json);
+                    //return json;
+                }
+            } else {
+                $("#responseTextArea").text("error");
+            }
+        }
+    };
+
+    var fd = new FormData();
+    fd.append('api_key', API_KEY);
+    fd.append('api_secret', API_SECRET);
+    fd.append('return_attributes', ATRIBUTOS);
+    xhr.open('POST', API_URL + '/compare');
+    fd.append('' + source1type, source1);
+    fd.append('' + source2type, source2);
+
+    xhr.send(fd);
+
+};
+
